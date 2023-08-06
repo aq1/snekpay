4240bc89254c1006e3fd7935c1a3047c094bc5b9
aq1 <aq1@aq1.aq1>
2023-08-07 00:10:24 +0300

    Change axiom import

--- src/lib/logs.ts
+++ src/lib/logs.ts
@@ -1,7 +1,7 @@
-import { Axiom } from '@axiomhq/js'
+import axiomjs from '@axiomhq/js'
 import { AXIOM_ORG_ID, AXIOM_TOKEN } from '$env/static/private'
 
-const axiom = new Axiom({
+const axiom = new axiomjs.Axiom({
 	token: AXIOM_TOKEN,
 	orgId: AXIOM_ORG_ID
 })
